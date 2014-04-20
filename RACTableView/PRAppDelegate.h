f74270bb5d20864b8d11d3b02e631851ac7f3688
Paul Robinson <paul@elasticrat.com>
2014-04-20 12:43:12 -0700

    Initial Commit

---
+++ RACTableView/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  RACTableView
+//
+//  Created by Paul Robinson on 2014-04-20.
+//  Copyright (c) 2014 Elastic Rat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RACTableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RACTableView
+//
+//  Created by Paul Robinson on 2014-04-20.
+//  Copyright (c) 2014 Elastic Rat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAppDelegate class]));
+    }
+}
