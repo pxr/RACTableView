3a75b163b2d9ea40d29a6a28b9cbb09db1f0ecf4
Paul Robinson <paul@elasticrat.com>
2014-04-20 15:12:40 -0700

    connecting kvoarray

--- RACTableView/ViewController.m
+++ RACTableView/ViewController.m
@@ -12,6 +12,8 @@
 
 @interface ViewController ()
 @property (nonatomic) ViewModel *viewModel;
+@property (nonatomic) UITableView *tableView;
+@property (nonatomic) NSArray *dataArray;
 @end
 
 @implementation ViewController
@@ -25,6 +27,8 @@
     
     [self bindToViewModel];
     
+    [self setupTableView];
+    
     [_viewModel testChanges];
     return self;
 }
@@ -34,16 +38,32 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)setupTableView {
+    self.tableView = [[UITableView alloc] initWithFrame:self.view.frame];
+    self.tableView.dataSource = self;
+    [self.view addSubview:self.tableView];
+}
+
 - (void)bindToViewModel {
     // We want the change info about the array changes too
     RACSignal *changeSignal = [self.viewModel rac_valuesAndChangesForKeyPath:@"holder.array" options: NSKeyValueObservingOptionNew| NSKeyValueObservingOptionOld observer:nil];
     [changeSignal subscribeNext:^(RACTuple *x){
-        NSArray *wholeArray = x.first;
+        self.dataArray = x.first;
         NSDictionary *changeDictionary = x.second;
         
-        NSLog(@"array: %@", wholeArray);
         NSLog(@"change: %@", changeDictionary);
     }];
+}
+
+#pragma mark - UITableViewDataSource
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return  [self.dataArray count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] init];
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    return cell;
 }
 
 
